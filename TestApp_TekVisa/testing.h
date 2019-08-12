64c5ecb5c823156c06b14a539cdcfc128646c922
Ivaylo Angelov <ivaylo_angelov@gmx.de>
2019-08-12 11:37:32 +0200

    first commit in new branch to get the building tools right

--- TestApp_TekVisa/testing.h
+++ TestApp_TekVisa/testing.h
@@ -2,6 +2,6 @@
 #include <visa.h>
 #include <visatype.h>
 
-//device name, if changed, alter here
+//device name, if changed, alter here.. TODO
 #define ViString DEVICE = "TODO";
 #define ViUInt32 BUFSIZE_DEFAULT = 1000;
